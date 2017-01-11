39bd115ab991e6fc296244187af1beaed1603c74
ZeinaDaghlas <zeina.doughlas@gmail.com>
2017-01-11 19:39:52 +0200

    Update FileSequenceReader.java

--- src/softwareconstruction/FileSequenceReader.java
+++ src/softwareconstruction/FileSequenceReader.java
@@ -1,3 +1,12 @@
+// from the DR // 
+
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -51,4 +60,28 @@ public class FileSequenceReader {
         return Integer.toBinaryString(b & 0xFF);
     }
 
+}
+
+
+
+
+//-------------------------------------------------------------------We have to change this class --------//
+/**
+ * Static class for reading from a file-sequence stream.
+
+ */
+public class FileSequenceReader {
+	/**
+	 * Returns the data from the next sub-file in the given file sequence stream.
+	 * <p>
+	 * If no sub-files remain, returns null. If the stream ends prematurely,
+	 * throws an EOFException.
+	 */
+	public static byte[] readOneFile(InputStream sequence)
+		throws IOException, EOFException {
+		// sequence files consist of a (4-byte) int giving the size of the sub-file,
+		// followed by the sub-file, followed by another size, followed by the sub-file,
+		// and so on until EOF
+		
+	}
 }
