dddcddc3f843a9384b66d57e9f689bfd3276bd56
ZeinaDaghlas <zeina.doughlas@gmail.com>
2017-01-27 17:02:03 +0200

    Update FileSequenceReader.java
    Final Code for this class

--- src/softwareconstruction/FileSequenceReader.java
+++ src/softwareconstruction/FileSequenceReader.java
@@ -1,76 +1,30 @@
-// from the DR // 
-
-import java.io.DataInputStream;
-import java.io.EOFException;
-import java.io.IOException;
-import java.io.InputStream;
-
-
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package softwareconstruction;
+package multipartdownloader;
 
 /**
  *
  * @author sallyishak
  */
 
+
+import org.apache.log4j.Logger;
+
 import java.io.*;
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
 
-public class FileSequenceReader {
-
-	public static byte[] readOneFile(InputStream sequence)
-		throws IOException, EOFException {
-	
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-
-        byte[] byteSizeFile = new byte[4];
-         if((sequence.read(byteSizeFile)) != -1)
-        {
-            String s="";
-
-            for(byte b:byteSizeFile)
-            s+=unsignedToBytes(b);
-
-            int size=Integer.parseInt(s, 2);
-
-
-            byte[] data = new byte[size];
-            int nRead = sequence.read(data, 0, data.length);
-            buffer.write(data,1, nRead);
-        }
-
-
-        buffer.flush();
-
-
-        return buffer.toByteArray();
-
-	}
-
-    public static String unsignedToBytes(byte b)
-    {
-     
-        return Integer.toBinaryString(b & 0xFF);
-    }
-
-}
-
-
-
-
-//------------------------------------------------------------We have to change this class --------//
 /**
  * Static class for reading from a file-sequence stream.
 
  */
 public class FileSequenceReader {
+
+    final static Logger logger = Logger.getLogger(FileSequenceReader.class);
+
 	/**
 	 * Returns the data from the next sub-file in the given file sequence stream.
 	 * <p>
@@ -82,7 +36,50 @@ public class FileSequenceReader {
 		// sequence files consist of a (4-byte) int giving the size of the sub-file,
 		// followed by the sub-file, followed by another size, followed by the sub-file,
 		// and so on until EOF
-	
-		
+
+        if(logger.isDebugEnabled())
+            logger.info("Start Call function readOneFile(...)");
+
+
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+        byte[] byteSizeFile = new byte[4];
+         if((sequence.read(byteSizeFile)) != -1)
+        {
+            String s="";
+
+            for(byte b:byteSizeFile)
+            s+=unsignedToBytes(b);
+
+            if(logger.isDebugEnabled())
+            logger.debug("size file in byte:"+byteSizeFile);
+
+            int size=Integer.parseInt(s, 2);
+
+            if(logger.isDebugEnabled())
+                logger.debug("size file in Integer:"+size);
+
+            byte[] data = new byte[size];
+            int nRead = sequence.read(data, 0, data.length);
+            buffer.write(data, 0, nRead);
+        }
+
+
+        buffer.flush();
+
+        if(logger.isDebugEnabled())
+            logger.info("End Call function readOneFile(...)");
+
+        return buffer.toByteArray();
+
 	}
+
+    public static String unsignedToBytes(byte b)
+    {
+        if(logger.isDebugEnabled())
+            logger.info("Call function unsignedToBytes(...)");
+
+        return Integer.toBinaryString(b & 0xFF);
+    }
+
 }
